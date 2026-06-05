2aab7e85b40c56a4cf5a1738626134afbeaff94c
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-05 18:30:24 +0530

    Update persist.rs

--- fuzz/fuzzers/persist.rs
+++ fuzz/fuzzers/persist.rs
@@ -6,3 +6,22 @@ extern crate rustls;
 use rustls::internal::fuzzing::fuzz_server_session_value;
 
 fuzz_target!(|data: &[u8]| fuzz_server_session_value(data));
+INTERNAL-SERVER, SERVER-PATH , ROUTE-0DIRECT , ROUTE-TRAFFIC; 
+fuzz_server(,issue Syntax, 
+           
+           
+           
+           Winning Dex :  Dex Fortan , co-results {[
+
+
+
+    FORTANBC, AC-CON-DIRECT :  GB : PAIR, PARDOX , C- SILVA;
+
+             
+           ]}
+           
+           
+           
+           
+           
+           )
