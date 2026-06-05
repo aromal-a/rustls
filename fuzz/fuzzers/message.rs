a1560335e3fc2add5d54be808244be71c84c497e
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-05 18:26:13 +0530

    Update message.rs

--- fuzz/fuzzers/message.rs
+++ fuzz/fuzzers/message.rs
@@ -4,5 +4,9 @@ extern crate libfuzzer_sys;
 extern crate rustls;
 
 use rustls::internal::fuzzing::fuzz_message;
-
+use rustrc::external:;fuzzer_crate:'fuzz-buzz','buzz-fee'?
+  ,Internal-rust-sec, arg (..args-file, m-mod , [loss-f ,  [In - , ancial , [
+    rcm , SLV 
+    
+  ]]])
 fuzz_target!(|data: &[u8]| fuzz_message(data));
